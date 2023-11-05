353201379a4cee1775acb266e29bdfced415836e
tgoodell <github@tgoodell.com>
2023-11-05 13:33:22 -0500

    Good first start

--- js/video.js
+++ js/video.js
@@ -1,11 +1,50 @@
-var video;
+var video = document.getElementById("player1");;
 
 window.addEventListener("load", function() {
 	console.log("Good job opening the window")
 
 });
 
-// document.querySelector("#play").addEventListener("click", function() {
-// 	console.log("Play Video");
-// });
+document.querySelector("#play").addEventListener("click", function() {
+	console.log("Play Video");
+	video.play()
+});
 
+document.querySelector("#pause").addEventListener("click", function() {
+	console.log("Pause Video");
+	video.pause()
+});
+
+document.querySelector("#slower").addEventListener("click", function() {
+	video.playbackRate = video.playbackRate * 0.9
+	console.log(video.playbackRate)
+});
+
+document.querySelector("#faster").addEventListener("click", function() {
+	video.playbackRate = video.playbackRate / 0.9
+	console.log(video.playbackRate)
+});
+
+document.querySelector("#skip").addEventListener("click", function() {
+	video.currentTime = video.currentTime + 10;
+	console.log(video.ended)
+	// video.onended = function() {
+	// 	video.currentTime = 0
+	// 	video.play();
+	// }
+});
+
+
+
+document.querySelector("#mute").addEventListener("click", function() {
+	console.log("Mute Video");
+	video.muted = true;
+});
+
+document.querySelector("#vintage").addEventListener("click", function() {
+	console.log("Old School Video");
+});
+
+document.querySelector("#orig").addEventListener("click", function() {
+	console.log("Original Video");
+});
